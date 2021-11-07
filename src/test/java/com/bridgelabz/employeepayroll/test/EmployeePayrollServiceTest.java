d81473acbafbcc2845d7fff7c9b25a17c4a9a55b
unknown <navyas211@gmail.com>
2021-11-07 15:33:02 +0530

    [NAVAYA]ADDED: Create an Employee Payroll service to store Employee payroll into a file.

--- src/test/java/com/bridgelabz/employeepayroll/test/EmployeePayrollServiceTest.java
+++ src/test/java/com/bridgelabz/employeepayroll/test/EmployeePayrollServiceTest.java
@@ -11,9 +11,9 @@ import com.bridgelabz.employeepayroll.EmployeePayrollService;
 public class EmployeePayrollServiceTest {
 	@Test
 	public void given3EmployeesWhenWrittenToFileShouldMatchNumberOfEmployeeEntries() {
-		EmployeePayroll[] arrayOfEmployees = { new EmployeePayroll(1, "Chaitra NS", 800000.0),
-				new EmployeePayroll(2, "Tushar Akhade", 850000.0),
-				new EmployeePayroll(3, "Geeta Biradar", 900000.0) };
+		EmployeePayroll[] arrayOfEmployees = { new EmployeePayroll(1, "Navaya Shree", 800000.0),
+				new EmployeePayroll(2, "Anokhi Sha", 850000.0),
+				new EmployeePayroll(3, "Lisha", 900000.0) };
 		EmployeePayrollService payrollServiceObject = new EmployeePayrollService(Arrays.asList(arrayOfEmployees));
 		payrollServiceObject.writeEmployeeDdate(EmployeePayrollService.IOService.FILE_IO);
 
